ba39e502a2bca62fd4b08011b719c80d96d7cd0c
Thiago Santos Magalhães <thiagosmfis@gmail.com>
2020-01-14 21:38:30 -0300

    Create InputController and Container classes

---
+++ Assets/Scripts/Container.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Container : MonoBehaviour
+{
+    [SerializeField]
+    private InputController inputController;
+
+    [SerializeField]
+    private ShootController shootController;
+
+    private void Start()
+    {
+        inputController.Initialize(shootController);
+    }
+}
---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    private ShootController shootController;
+
+    private Ray ray;
+    private RaycastHit hit;
+
+    public void Initialize(ShootController shootController)
+    {
+        this.shootController = shootController;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            RaycastToField();
+    }
+
+    private void RaycastToField()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            //Do something
+        }
+    }
+}
